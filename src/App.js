111a071d701271b45dc2a7847732ef2f8c8fae6c
Jayant <jayantbihare@gmail.com>
2022-01-01 15:09:26 +0530

    ReactCourseProject

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+function App() {
+  const expenses = [
+    {id: "e1", title: "Toilet Paper", amount: 94.12, date: new Date(2020, 7, 1)},
+    {id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12)},
+    {id: "e3", title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28)},
+    {id: "e4", title: "New Desk (Wooden)", amount: 450, date: new Date(2021, 5, 12)},
+    {id: "e5", title: "Diapers", amount: 505, date: new Date(2021, 4, 14)},
+    {id: "e6", title: "Food Items", amount: 4505, date: new Date(2021, 3, 25)}
+  ];
+ 
+  return (
+    <div>
+      <NewExpense />
+      <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
